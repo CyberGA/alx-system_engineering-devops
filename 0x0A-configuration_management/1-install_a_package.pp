183aac00344351a454e438ae21eae19fb191f710
CyberGA <etomu.joshua@gmail.com>
2024-01-19 19:00:21 -0800

    fix: installing flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
+# Using Puppet, install flask from pip3.
+
+package {'python3-pip':
+  ensure => installed,
+}
+
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+package {'werkzeug':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
