38b8fe584f1ea8e8377cae7142a3ef45a012a40a
CyberGA <etomu.joshua@gmail.com>
2024-03-05 16:51:48 -0800

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
         return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": '0x16-api_advanced:project:v1.0.0(by /u/CyberGA)'
+        "User-Agent": '0x16-api_advanced:project:v1.0.0(by /u/cyber_ga)'
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200 and not response.is_redirect:
