9af11e23b78731fbb83ac342abf0faefd43c5fbf
CyberGA <etomu.joshua@gmail.com>
2024-03-05 16:21:32 -0800

    fix query

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:v1.0.0(by /u/CyberGA)"
+        "User-Agent": '0x16-api_advanced:project:v1.0.0(by /u/CyberGA)'
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200 and not response.is_redirect:
